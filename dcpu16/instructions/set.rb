256900ebaf7dc9dc9cb5cd2480048dd8b5d7c90d
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-04-11 22:57:19 +0200

    Refactor value setting

--- dcpu16/instructions/set.rb
+++ dcpu16/instructions/set.rb
@@ -11,30 +11,7 @@ module DCPU16
 
       def execute(emulator)
         value = ValueSet.get emulator, @b, @next_word_b
-
-        case @a
-        when 0x00..0x07
-          emulator.registers[@a] = value
-        when 0x08..0x0F
-          emulator.memory[emulator.registers[@a - 0x08]] = value
-        when 0x10..0x17
-          emulator.memory[@next_word_a + emulator.registers[@a - 0x10]] = value
-        when 0x1A
-          emulator.stack.push value
-        when 0x1b
-          emulator.sp = value
-        when 0x1c
-          emulator.pc = value
-        when 0x1d
-          p "You tried to assign 0!"
-        when 0x1E
-          emulator.memory[@next_word_a] = value
-        when 0x1F
-          p "You tried to assign a literal value!"
-        when 0x20..0x3f
-          p "You tried to assign a literal value!"
-        end
-
+        ValueSet.set emulator, @a, @next_word_a, value
       end
     end
   end
--- dcpu16/value_set.rb
+++ dcpu16/value_set.rb
@@ -31,6 +31,31 @@ module DCPU16
           raise "Unknown value: #{code.to_s(16)}!"
         end
     end
+
+    def self.set(emulator, code, next_word, value)
+        case code
+        when 0x00..0x07
+          emulator.registers[code] = value
+        when 0x08..0x0F
+          emulator.memory[emulator.registers[code - 0x08]] = value
+        when 0x10..0x17
+          emulator.memory[next_word + emulator.registers[code - 0x10]] = value
+        when 0x1A
+          emulator.stack.push value
+        when 0x1b
+          emulator.sp = value
+        when 0x1c
+          emulator.pc = value
+        when 0x1d
+          p "You tried to assign 0!"
+        when 0x1E
+          emulator.memory[next_word] = value
+        when 0x1F
+          p "You tried to assign a literal value!"
+        when 0x20..0x3f
+          p "You tried to assign a literal value!"
+        end
+    end
   end
 
 end
