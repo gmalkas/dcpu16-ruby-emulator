2a440f2e30ec1fe8a5e9ddb714154cd4dfa10959
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-04-12 15:09:31 +0200

    Fix syntax error

--- dcpu16/value_set.rb
+++ dcpu16/value_set.rb
@@ -7,7 +7,7 @@ module DCPU16
     def self.get(emulator, code, next_word)
         case code     
         when 0x00..0x07
-          emulator.registers[codeb]
+          emulator.registers[code]
         when 0x08..0x0F
           emulator.memory.fetch(emulator.registers[code - 0x08])
         when 0x10..0x17
@@ -25,23 +25,26 @@ module DCPU16
         when 0x1E
           emulator.memory.fetch(next_word)
         when 0x1F
-          next_word
+          Memory.to_bin next_word
         when 0x20..0x3f
-          code - 0x20
+          Memory.to_bin (code - 0x20)
         else
           raise "Unknown value: #{code.to_s(16)}!"
         end
     end
 
+    #
+    # `code` and `next_word` must be integers
+    # `value` must be a binary string
+    #
     def self.set(emulator, code, next_word, value)
-        bin_value = Memory.to_bin value
         case code
         when 0x00..0x07
-          emulator.registers[code] = bin_value
+          emulator.registers[code] = value
         when 0x08..0x0F
-          emulator.memory[emulator.registers[code - 0x08]] = bin_value
+          emulator.memory.insert(emulator.registers[code - 0x08], value)
         when 0x10..0x17
-          emulator.memory[next_word + emulator.registers[code - 0x10]] = value
+          emulator.memory.insert(next_word + emulator.registers[code - 0x10], value)
         when 0x1A
           emulator.stack.push value
         when 0x1b
@@ -51,7 +54,7 @@ module DCPU16
         when 0x1d
           p "You tried to assign 0!"
         when 0x1E
-          emulator.memory[next_word] = value
+          emulator.memory.insert(next_word, value)
         when 0x1F
           p "You tried to assign a literal value!"
         when 0x20..0x3f
