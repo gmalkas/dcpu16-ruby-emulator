4488c1a687e4a524e8114e31eebed53a0fe0d6ab
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-04-12 16:41:08 +0200

    Add AND, BOR and XOR

---
+++ dcpu16/instructions/bor.rb
@@ -0,0 +1,21 @@
+require_relative '../value_set'
+require_relative '../basic_instruction'
+
+module DCPU16
+  module Instructions
+    class Bor < BasicInstruction
+      def initialize
+        @opcode = 0xa
+      end
+
+      def execute(emulator)
+        b = ValueSet.get emulator, @b, @next_word_b
+        a = ValueSet.get emulator, @a, @next_word_a
+        a_i = Memory.to_i a
+        b_i = Memory.to_i b
+        result = Memory.to_bin (a_i | b_i)
+        ValueSet.set emulator, @a, @next_word_a, result
+      end
+    end
+  end
+end
---
+++ dcpu16/instructions/xor.rb
@@ -0,0 +1,22 @@
+require_relative '../value_set'
+require_relative '../basic_instruction'
+
+module DCPU16
+  module Instructions
+    class Xor < BasicInstruction
+      def initialize
+        @opcode = 0xb
+      end
+
+      def execute(emulator)
+        b = ValueSet.get emulator, @b, @next_word_b
+        a = ValueSet.get emulator, @a, @next_word_a
+        a_i = Memory.to_i a
+        b_i = Memory.to_i b
+        result = Memory.to_bin (a_i ^ b_i)
+        ValueSet.set emulator, @a, @next_word_a, result
+      end
+    end
+  end
+end
+
